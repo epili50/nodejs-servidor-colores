87ddb60242afac24f3bba0ad2afde83535d44a88
epili50 <epili50@gmail.com>
2024-06-26 20:41:19 +0200

    Terminado (lo hice en Solucion Oscar)

--- Solucion-Oscar.js
+++ Solucion-Oscar.js
@@ -20,6 +20,8 @@ const colors = [
 
 // Módulo interno
 const http = require('http');
+const fs = require('fs');
+const querystring = require('querystring');
 
 // Creamos servidor y lo asigno a una variable
 const server = http.createServer((req, res) => {
@@ -29,7 +31,37 @@ const server = http.createServer((req, res) => {
     const { url } = req;
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
 
-    if (url.startsWith("/color")) {
+    // Caso que tiene ?variant
+    if (url.startsWith("/color?")) {
+        // Usamos el operador de desestructuración para asignar a la variable path la primera posición del array y a la variable queryString la segunda posición del array
+        const [path, queryString] = req.url.split('?');
+        const qs = querystring.parse(queryString);
+        console.log("🚀 ~ file: app.js:39 ~ server ~ qs:", qs);
+
+        // 1. Utilizar el método de array adecuado para buscar la variante del color en el array 'colors'
+        const chooseColor = colors.find(c => c.variant.toLowerCase() == qs.variant.toLowerCase());
+        console.log("🚀 ~ file: Solucion-Oscar.js:43 ~ server ~ chooseColor:", chooseColor)
+       
+        // 2. Si encuentra el color, devolverlo tal y cómo lo hacemos aquí         res.end(`<p style="color: ${hex}">${hex}</p>`)
+        if(chooseColor){
+            res.write(`<p style="color: ${chooseColor.hex}">${chooseColor.hex}</p>`);
+        }
+        // 3. Si NO encuentra el color, mandarle uno al azar.  Mandarle también un mensaje diciendo que la variante de color elegida NO EXiste. Opcionalmente podéis enviarle el array de colores que puede usar
+
+        else{
+            res.write(`El color ${qs.variant} no existe. Te muestro un color al azar`)
+            // obtener un color aleatório
+            const indexRandomColor = Math.floor(Math.random() * colors.length);
+            const randomColor = colors[indexRandomColor];
+
+            const { hex } = randomColor;
+            res.write(`<p style="color: ${hex}">${hex}</p>`)
+        }
+
+        res.end();
+    }
+
+    else if (url == "/color") {
 
         // Iteración 3: Comprobar si me han pasado una queryString o no. En caso de que si: obtener el color en función del ?variant=Vermillion. En caso contrario obtener un color aleatório
 
@@ -39,9 +71,10 @@ const server = http.createServer((req, res) => {
 
         // me quedo con la propiedad .hex del color
         const { hex } = randomColor;
+        res.statusCode = 200;
         res.end(`<p style="color: ${hex}">${hex}</p>`)
 
-    } else {
+    } else if (url == "/") {
 
         // Especificar que vamos a enviar un html
         // Tenemos que especificar que la codificación es utf-8
@@ -50,6 +83,13 @@ const server = http.createServer((req, res) => {
         res.write('<h1>Bienvenido al servidor de colores</h1>');
         res.write('<p>Haz una petición a /color para obtener un color aleatório</p>');
         res.end();
+    } else {
+        // el recurso/endpoint/ruta que intentas acceder no existe
+        res.statusCode = 404;
+        // le enviamos el fichero 404.html
+        // Voy a leer el fichero con un método síncrono
+        const htmlContent = fs.readFileSync("./404.html");
+        res.end(htmlContent);
     }
 });
 
